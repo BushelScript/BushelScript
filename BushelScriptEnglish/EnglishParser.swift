e77251ed7c445da36b8dac582a71f2d389883008
Ian Gregory <justcheesy@outlook.com>
2020-01-08 01:02:35 -0500

    Wait… "get" does belong here! sigh, bedtime

--- BushelScriptEnglish/EnglishParser.swift
+++ BushelScriptEnglish/EnglishParser.swift
@@ -21,6 +21,8 @@ public final class EnglishParser: BushelLanguage.SourceParser {
     }
     
     private lazy var mainDictionary: [TermDescriptor] = [
+        CommandDescriptor(.get, name: TermName("get")),
+        
         PropertyDescriptor(.properties, name: TermName("properties")),
         PropertyDescriptor(.type, name: TermName("type")),
         PropertyDescriptor(.index, name: TermName("index")),
