00342178aeee91dfdd92c96cc824ecbfcc69ef56
Ian Gregory <justcheesy@outlook.com>
2020-02-01 10:18:38 -0500

    (2) Only embed core frameworks in app package Also embed command-line tool in app package (so it can link with frameworks there), and add an installation window to the Editor app.

--- make-pkg.sh
+++ make-pkg.sh
@@ -18,7 +18,6 @@ echo "Building to ${INSTALL_DIR}"
 echo 'Installing.'
 function install {
 	xcodebuild install -workspace Bushel.xcworkspace -scheme BushelScript\ Editor DSTROOT="$INSTALL_DIR" # Includes all language modules.
-	xcodebuild install -workspace Bushel.xcworkspace -scheme bushelscript DSTROOT="$INSTALL_DIR"
 	if [ $? -ne 0 ]
 	then
 		echo 'xcodebuild install failed; not creating a pkg.'
