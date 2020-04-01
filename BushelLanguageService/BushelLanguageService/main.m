9e9a3c86902028449b4cf45326f78f4214b824fc
Ian Gregory <justcheesy@outlook.com>
2020-04-01 01:29:05 -0400

    Update end user-facing copyright year

--- BushelLanguageService/BushelLanguageService/main.m
+++ BushelLanguageService/BushelLanguageService/main.m
@@ -4,7 +4,7 @@
 //  BushelLanguageService
 //
 //  Created by Ian Gregory on 27-08-2019.
-//  Copyright © 2019 Ian Gregory. All rights reserved.
+//  Copyright © 2019–2020 Ian A. Gregory. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- BushelLanguageService/BushelLanguageServiceConnectionCreation/BushelLanguageServiceConnectionCreation.h
+++ BushelLanguageService/BushelLanguageServiceConnectionCreation/BushelLanguageServiceConnectionCreation.h
@@ -3,7 +3,7 @@
 //  BushelLanguageServiceConnectionCreation
 //
 //  Created by Ian Gregory on 27-08-2019.
-//  Copyright © 2019 Ian Gregory. All rights reserved.
+//  Copyright © 2019–2020 Ian A. Gregory. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
