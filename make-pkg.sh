84f3e81a70f653ad1c5b524aba6c54f35d097e21
Ian Gregory <justcheesy@outlook.com>
2020-10-10 21:11:28 -0400

    Remove LLVMSwift package entirely

--- make-pkg.sh
+++ make-pkg.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-# Set up LLVM in case that hasn't been done yet.
-./set-up-llvm.sh
-
 function get_project_version {
 	xcodebuild -showBuildSettings -workspace Bushel.xcworkspace -scheme BushelScript\ Editor | grep MARKETING_VERSION | head -1 | tr -d '[:alpha:][:space:]_='
 }
@@ -46,12 +43,6 @@ git clone --depth=1 'https://github.com/BushelScript/applescript-stdlib.git' "$A
 mkdir -p "$APPLESCRIPT_STDLIB_INSTALL_DIR"
 cp -r "$APPLESCRIPT_STDLIB_CLONE_DIR"/*.scptd "$APPLESCRIPT_STDLIB_INSTALL_DIR"
 rm -rf "$APPLESCRIPT_STDLIB_CLONE_DIR"
-
-echo "Temporary measure: copying LLVM's libc++.1.dylib to install dir."
-LLVM_LIB_DIR="$(brew --prefix llvm@9)/lib"
-mkdir -p "$INSTALL_DIR/$LLVM_LIB_DIR"
-LLVM_LIBCPP="$LLVM_LIB_DIR/libc++.1.dylib"
-cp "$LLVM_LIBCPP" "$INSTALL_DIR/$LLVM_LIBCPP"
 
 # Make the installer package.
 pkgbuild --root "$INSTALL_DIR" --identifier "$PKG_IDENTIFIER" --version "$PKG_VERSION" "$PKG_FILENAME"
--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-# Set up LLVM in case that hasn't been done yet.
-./set-up-llvm.sh
-
 # Run tests.
 echo 'Testing.'
 function test {
