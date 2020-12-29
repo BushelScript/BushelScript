de7581b0327c76785b6fe471457e18b4a6ebb9c6
Ian Gregory <justcheesy@outlook.com>
2020-12-29 16:24:57 -0500

    Tweaks/fixes for complex search algo

--- BushelLanguage/Utilities/Terminology.swift
+++ BushelLanguage/Utilities/Terminology.swift
@@ -34,47 +34,45 @@ public func findComplexTermName(from dictionary: TermNameTraversalTable, in sour
     // Advance endIndex to 1 past the end of each word in the line until
     // a term name matches those words, or we reach the end of the line.
     var endIndex = line.startIndex
+    var largestMatchEndIndex = endIndex
     while true {
-        let subsequentWordsStartIndex = endIndex
+        func largestMatch() -> (termString: Substring, termName: TermName?) {
+            let matchedSource = line[..<largestMatchEndIndex]
+            return (matchedSource, matchedSource.isEmpty ? nil : TermName(matchedSource))
+        }
+        
         endIndex =
             line[endIndex...]
             .drop(while: { $0.isWhitespace })
             .drop(while: { !$0.isWhitespace })
             .startIndex
+        
         guard let subsequentWords = dictionary[TermName(line[..<endIndex])] else {
-            // There are no possible longer matches of what we've already matched.
-            // Return the last match (or no match if there is no last match).
-            let matchedSource = line[..<subsequentWordsStartIndex]
-            return (matchedSource, matchedSource.isEmpty ? nil : TermName(matchedSource))
+            // There are no possible larger matches than whatever we've already matched.
+            return largestMatch()
         }
+        
         if subsequentWords.contains(TermName([])) {
             // Can match this iteration.
-            if subsequentWords.count == 1 || endIndex == line.endIndex {
-                // Exact match with no possible longer matches.
-                let matchedSource = line[..<endIndex]
-                return (matchedSource, TermName(matchedSource))
-            } else {
-                // We could have matched this iteration,
-                // but longer matches are also possible.
-                continue
+            largestMatchEndIndex = endIndex
+            
+            if subsequentWords.count == 1 {
+                // No possible larger matches.
+                return largestMatch()
             }
-        } else {
-            // Cannot possibly match this iteration.
-            if endIndex == line.endIndex {
-                // No matches possible.
-                return (line[..<line.startIndex], nil)
-            } else {
-                // Possible match in future iteration.
-                continue
-            }
+            
+            // If we get here, a larger match in a future iteration is possible.
+            // (If we find that there are no larger matches, however,
+            // this match will be used.)
         }
+        
+        if endIndex == line.endIndex {
+            // No future iterations possible.
+            return largestMatch()
+        }
+        
+        // If we get here, there is a possible match in a future iteration.
     }
-    
-    // TMR: This is slow because we are not backing out early enough.
-    //      Need to ideally filter the set word by word, and abort
-    //      the moment the set reaches 1 matching element, or 0 elements.
-    //      Might use hash table to expedite reaching that new set (precompute them all).
-    // ["a": ["b", "c", "b c"], "a b": ["", "c"]]
 }
     
 public func buildTraversalTable<TermNames: Collection>(
