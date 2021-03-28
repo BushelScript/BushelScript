363f911e6d5b5d5cd170c4a0f6a10c96886117fe
Ian Gregory <justcheesy@outlook.com>
2021-03-28 14:12:57 -0400

    alert: Cap message text height, allow scrolling

--- BushelGUIHost/Alerts/AlertWC.swift
+++ BushelGUIHost/Alerts/AlertWC.swift
@@ -2,8 +2,37 @@ import Cocoa
 
 final class AlertWC: NSWindowController {
     
+    @IBOutlet weak var messageTextView: NSTextView!
+    
+    var standardHeight: CGFloat = 300
+    override func awakeFromNib() {
+        guard let window = window else {
+            return
+        }
+        standardHeight = window.frame.height
+    }
+    
     @objc dynamic var heading: String?
-    @objc dynamic var message: String?
+    @objc dynamic var message: String? {
+        didSet {
+            // Adjust window height to accomodate message text,
+            // up to the screen's safe height limit.
+            // (Any further text will be scrollable.)
+            if let message = message, let window = window {
+                let messageTextHeight = (message as NSString).size(withAttributes: messageTextView.typingAttributes).height
+                let screenHeight = window.screen?.visibleFrame.height ?? standardHeight
+                let newWindowHeight = min(standardHeight + messageTextHeight, screenHeight)
+                let newWindowFrame = CGRect(
+                    origin: window.frame.origin,
+                    size: CGSize(
+                        width: window.frame.width,
+                        height: newWindowHeight
+                    )
+                )
+                window.setFrame(newWindowFrame, display: true)
+            }
+        }
+    }
     
     var response: String?
     
