abee2691e7debcaf2450789fe467ebdfeace4425
Ian Gregory <justcheesy@outlook.com>
2021-05-20 17:46:48 -0400

    CLT: Fix REPL printing and error formatting

--- bushelscript/bushelscript/ToolInvocation.swift
+++ bushelscript/bushelscript/ToolInvocation.swift
@@ -22,7 +22,6 @@ private struct InvocationState {
     var storedLanguageModule: (module: LanguageModule, language: String)?
     var storedParser: (parser: SourceParser, module: LanguageModule)?
     var rt = Runtime(currentApplicationBundleID: "com.justcheesy.BushelGUIHost")
-    var lastResult: RT_Object?
     
 }
 
@@ -30,25 +29,24 @@ extension ToolInvocation {
     
     func run() throws {
         var state = InvocationState()
+        var lastResult: RT_Object?
         if !scriptLines.isEmpty {
-            try run(&state, source: scriptLines.map { String($0) }.joined(separator: "\n"), fileName: "<command-line>", url: nil)
+            lastResult = run(&state, source: scriptLines.map { String($0) }.joined(separator: "\n"), fileName: "<command-line>", url: nil)
         }
         if !files.isEmpty {
             for file in files {
                 let file = String(file)
-                try run(&state, source: try String(contentsOfFile: file), fileName: file, url: URL(fileURLWithPath: file))
+                lastResult = run(&state, source: try String(contentsOfFile: file), fileName: file, url: URL(fileURLWithPath: file))
             }
         }
         if interactive {
             try runREPL(&state)
-        } else {
-            if printResult {
-                print(state.lastResult!)
-            }
+        } else if printResult, let lastResult = lastResult {
+            print(lastResult)
         }
     }
     
-    private func run(_ state: inout InvocationState, source: String, fileName: String, url: URL?) throws {
+    private func run(_ state: inout InvocationState, source: String, fileName: String, url: URL?) -> RT_Object? {
         var source = source
         var language = self.language
         
@@ -73,10 +71,19 @@ extension ToolInvocation {
         
         do {
             let program = try parser(&state, for: language).parse(source: source, at: url)
-            state.lastResult = try state.rt.run(program)
-        } catch let error as ParseErrorProtocol {
-            print(error: error, in: source, fileName: fileName)
+            return try state.rt.run(program)
+        } catch {
+            if let error = error as? (Located & Error) {
+                printErrorMessage(error.localizedDescription, in: source, at: error.location, fileName: fileName)
+                printLocationSnippet(for: error.location, in: source, indentation: 4, withMarker: true)
+            } else {
+                printErrorMessage(error.localizedDescription, fileName: fileName)
+            }
+            if let error = error as? ParseErrorProtocol {
+                printSourceFixes(error.fixes, in: source)
+            }
         }
+        return nil
     }
     
     private func runREPL(_ state: inout InvocationState) throws {
@@ -94,7 +101,9 @@ extension ToolInvocation {
             guard !(Substring(line).trimmingWhitespace() == ":exit") else {
                 return
             }
-            try run(&state, source: line, fileName: "<repl>", url: nil)
+            if let result = run(&state, source: line, fileName: "<repl>", url: nil) {
+                print(result)
+            }
         }
         print()
     }
@@ -131,31 +140,13 @@ extension ToolInvocation {
     
 }
 
-private func print(error: ParseErrorProtocol, in source: String, fileName: String) {
-    let location = error.location
-
+private func printErrorMessage(_ message: String, fileName: String) {
+    print("error in \(fileName): \(message)")
+}
+private func printErrorMessage(_ message: String, in source: String, at location: SourceLocation, fileName: String) {
     let lines = location.lines(in: source).colloquialStringRepresentation
     let columns = location.columns(in: source).colloquialStringRepresentation
-    print("error in \(fileName):\(lines):\(columns): \(error)") // FIXME: format the error
-    
-    printLocationSnippet(for: location, in: source, indentation: 4, withMarker: true)
-    
-    for fix in error.fixes {
-        print("  > possible fix: \(fix.contextualDescription(in: Substring(source)).replacingOccurrences(of: "\n", with: "\\n"))")
-        
-        var fixedSource = source
-        var impacts: [FixImpact] = []
-        do {
-            try fix.apply(to: &fixedSource, initialSource: source, impacts: &impacts)
-        } catch {
-            print("error while applying fix: \(error)") // TODO: Move this code to Bushel and make this print an os_log
-        }
-        
-        let lowestIndex = fix.locations.reduce(fix.locations[0].range.lowerBound) { $1.range.lowerBound < $0 ? $1.range.lowerBound : $0 }
-        let highestIndex = fix.locations.reduce(fix.locations[0].range.upperBound) { $1.range.upperBound > $0 ? $1.range.upperBound : $0 }
-        let combinedLocation = SourceLocation(lowestIndex..<highestIndex, source: fixedSource)
-        printLocationSnippet(for: combinedLocation, in: fixedSource, indentation: 8, withMarker: true)
-    }
+    print("error in \(fileName):\(lines):\(columns): \(message)")
 }
 
 private func printLocationSnippet(for location: SourceLocation, in source: String, indentation indentCount: Int, withMarker: Bool) {
@@ -179,10 +170,6 @@ private func printLocationSnippet(for location: SourceLocation, in source: Strin
     }
 }
 
-private func description<S: StringProtocol>(of range: Range<S.Index>, mappedOnto string: S) -> String {
-    return "[\(string.distance(from: string.startIndex, to: range.lowerBound)), \(string.distance(from: string.startIndex, to: range.upperBound)))"
-}
-
 extension Range where Bound: SignedInteger {
     
     var colloquialStringRepresentation: String {
@@ -192,4 +179,23 @@ extension Range where Bound: SignedInteger {
         return "\(lowerBound)–\(upperBound - 1)"
     }
     
+}
+
+private func printSourceFixes(_ fixes: [SourceFix], in source: String) {
+    for fix in fixes {
+        print("  > possible fix: \(fix.contextualDescription(in: Substring(source)).replacingOccurrences(of: "\n", with: "\\n"))")
+        
+        var fixedSource = source
+        var impacts: [FixImpact] = []
+        do {
+            try fix.apply(to: &fixedSource, initialSource: source, impacts: &impacts)
+        } catch {
+            print("error while applying fix: \(error)") // TODO: Move this code to Bushel and make this print an os_log
+        }
+        
+        let lowestIndex = fix.locations.reduce(fix.locations[0].range.lowerBound) { $1.range.lowerBound < $0 ? $1.range.lowerBound : $0 }
+        let highestIndex = fix.locations.reduce(fix.locations[0].range.upperBound) { $1.range.upperBound > $0 ? $1.range.upperBound : $0 }
+        let combinedLocation = SourceLocation(lowestIndex..<highestIndex, source: fixedSource)
+        printLocationSnippet(for: combinedLocation, in: fixedSource, indentation: 8, withMarker: true)
+    }
 }
