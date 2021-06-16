d1df9f2ea6186f584a4228a7a5e0f4449f3a93eb
Ian Gregory <justcheesy@outlook.com>
2021-06-16 03:03:51 -0400

    Remove useless build script

--- resolve-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-# Accessible from parent shell if this script is source'd.
-PACKAGES_DIR="${PACKAGES_DIR:-$(pwd)/swift-packages}"
-echo "Resolving package dependencies to ${PACKAGES_DIR}"
-
-# Resolve Swift package dependencies.
-xcodebuild -resolvePackageDependencies -workspace Bushel.xcworkspace -scheme BushelScript\ Editor -clonedSourcePackagesDirPath "$PACKAGES_DIR"
