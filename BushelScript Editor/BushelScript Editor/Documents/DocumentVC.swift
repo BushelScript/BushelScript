81a251986b9b5a90d9a60282d4a68383d86f78c8
Ian Gregory <justcheesy@outlook.com>
2021-06-23 12:00:46 -0400

    Editor: Fix off-by-one when entering newline on first line

--- BushelScript Editor/BushelScript Editor/Documents/DocumentVC.swift
+++ BushelScript Editor/BushelScript Editor/Documents/DocumentVC.swift
@@ -367,7 +367,10 @@ extension DocumentVC: NSTextViewDelegate {
             if lineBreakNSRange.location == NSNotFound {
                 lineBreakNSRange = NSRange(location: 0, length: 0)
             }
-            var firstNonspaceNSRange = (textStorage.string as NSString).rangeOfCharacter(from: CharacterSet.whitespaces.inverted, options: [], range: NSRange(location: lineBreakNSRange.location + 1, length: selectedRange.location - (lineBreakNSRange.location + 1)))
+            var firstNonspaceNSRange = NSRange(location: NSNotFound, length: 0)
+            if selectedRange.location > lineBreakNSRange.location {
+                firstNonspaceNSRange = (textStorage.string as NSString).rangeOfCharacter(from: CharacterSet.whitespaces.inverted, options: [], range: NSRange(location: lineBreakNSRange.location + 1, length: selectedRange.location - (lineBreakNSRange.location + 1)))
+            }
             if firstNonspaceNSRange.location == NSNotFound {
                 firstNonspaceNSRange = NSRange(location: selectedRange.location, length: 0)
             }
