e22a71038dc880aa3493b49f6da0efbda6fbea92
Ian Gregory <justcheesy@outlook.com>
2021-06-23 12:10:53 -0400

    Repair integer comparison and arithmetic

--- BushelRT/Runtime Objects/RT_Integer.swift
+++ BushelRT/Runtime Objects/RT_Integer.swift
@@ -27,6 +27,49 @@ public class RT_Integer: RT_Object {
         value != 0
     }
     
+    public override func compare(with other: RT_Object) -> ComparisonResult? {
+        if let other = other as? RT_Integer {
+            return self.value <=> other.value
+        } else if let other = other.coerce(to: RT_Real.self) {
+            return Double(self.value) <=> other.value
+        }
+        return nil
+    }
+    
+    public override func adding(_ other: RT_Object) -> RT_Object? {
+        if let other = other as? RT_Integer {
+            return RT_Integer(rt, value: self.value + other.value)
+        } else if let other = other.coerce(to: RT_Real.self) {
+            return RT_Real(rt, value: Double(self.value) + other.value)
+        }
+        return nil
+    }
+    
+    public override func subtracting(_ other: RT_Object) -> RT_Object? {
+        if let other = other as? RT_Integer {
+            return RT_Integer(rt, value: self.value - other.value)
+        } else if let other = other.coerce(to: RT_Real.self) {
+            return RT_Real(rt, value: Double(self.value) - other.value)
+        }
+        return nil
+    }
+    
+    public override func multiplying(by other: RT_Object) -> RT_Object? {
+        if let other = other as? RT_Integer {
+            return RT_Integer(rt, value: self.value * other.value)
+        } else if let other = other.coerce(to: RT_Real.self) {
+            return RT_Real(rt, value: Double(self.value) * other.value)
+        }
+        return nil
+    }
+    
+    public override func dividing(by other: RT_Object) -> RT_Object? {
+        if let other = other.coerce(to: RT_Real.self) {
+            return RT_Real(rt, value: Double(self.value) / other.value)
+        }
+        return nil
+    }
+    
     public override func coerce(to type: Reflection.`Type`) -> RT_Object? {
         if type.isA(rt.reflection.types[.real]) {
             return RT_Real(rt, value: Double(value))
