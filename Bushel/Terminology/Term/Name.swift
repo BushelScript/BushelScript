fdd701e3d72d18b356541ada92c8afc1262cb1dd
Ian Gregory <justcheesy@outlook.com>
2021-07-03 13:18:05 -0400

    Reorganize Term.Name

--- Bushel/Terminology/Term/Name.swift
+++ Bushel/Terminology/Term/Name.swift
@@ -1,12 +1,9 @@
 import Foundation
 
-// MARK: Definition
 extension Term {
     
     /// The user-facing name of a term, stored in a normalized representation.
-    public struct Name: Hashable, Codable, CustomStringConvertible {
-        
-        public static let separator = " "
+    public struct Name: Hashable, Codable {
         
         public init(_ words: [String]) {
             self.words = words
@@ -14,51 +11,72 @@ extension Term {
         
         public var words: [String] = []
         
-        public init<S: StringProtocol>(_ string: S) where S.SubSequence == Substring {
-            self.init(Term.Name.words(in: string))
-        }
-        
-        public static func words<S: StringProtocol>(in string: S) -> [String] where S.SubSequence == Substring {
-            var words: [String] = []
-            var word = ""
-            for c in string {
-                let isWhitespace = c.isWhitespace
-                let isWordBreaking = isWhitespace || c.isWordBreakingPunctuation
-                if isWordBreaking, !word.isEmpty {
-                    words.append(word)
-                    word = ""
-                }
-                if !isWhitespace {
-                    if isWordBreaking {
-                        words.append(String(c))
-                    } else {
-                        word.append(c)
-                    }
-                }
-            }
-            if !word.isEmpty {
+    }
+    
+}
+
+// MARK: String → term name
+extension Term.Name {
+    
+    public init<S: StringProtocol>(_ string: S) where S.SubSequence == Substring {
+        self.init(Term.Name.words(in: string))
+    }
+    
+    public static func words<S: StringProtocol>(in string: S) -> [String] where S.SubSequence == Substring {
+        var words: [String] = []
+        var word = ""
+        for c in string {
+            let isWhitespace = c.isWhitespace
+            let isWordBreaking = isWhitespace || c.isWordBreakingPunctuation
+            if isWordBreaking, !word.isEmpty {
                 words.append(word)
+                word = ""
             }
-            return words
-        }
-        
-        public static func nextWord<S: StringProtocol>(in string: S) -> String? where S.SubSequence == Substring {
-            words(in: string).first
-        }
-        
-        public var normalized: String {
-            get {
-                words.joined(separator: Term.Name.separator)
-            }
-            set {
-                self = Term.Name(newValue)
+            if !isWhitespace {
+                if isWordBreaking {
+                    words.append(String(c))
+                } else {
+                    word.append(c)
+                }
             }
         }
-        
-        public var description: String {
-            normalized
+        if !word.isEmpty {
+            words.append(word)
         }
-        
+        return words
+    }
+    
+    public static func nextWord<S: StringProtocol>(in string: S) -> String? where S.SubSequence == Substring {
+        words(in: string).first
+    }
+    
+}
+
+// MARK: Term name → normalized string
+extension Term.Name: CustomStringConvertible {
+    
+    public static let wordSeparator = " "
+    
+    public var description: String {
+        normalized
+    }
+    
+    public var normalized: String {
+        get {
+            words.joined(separator: Term.Name.wordSeparator)
+        }
+        set {
+            self = Term.Name(newValue)
+        }
+    }
+    
+}
+
+// MARK: Comparable
+extension Term.Name: Comparable {
+    
+    public static func < (lhs: Term.Name, rhs: Term.Name) -> Bool {
+        lhs.words < rhs.words
     }
     
 }
@@ -82,11 +100,3 @@ public let wordBreakingPunctuationCharacters: CharacterSet =
 public let wordBreakingCharacters: CharacterSet =
     CharacterSet.whitespacesAndNewlines
     .union(wordBreakingPunctuationCharacters)
-
-extension Term.Name: Comparable {
-    
-    public static func < (lhs: Term.Name, rhs: Term.Name) -> Bool {
-        lhs.words < rhs.words
-    }
-    
-}
