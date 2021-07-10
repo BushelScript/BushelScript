2bdc518f44203063b9262472a1c533b743dfadbc
Ian Gregory <justcheesy@outlook.com>
2021-07-10 19:02:04 -0400

    Cache: Fix properly enforcing access queue restrictions

--- Bushel/Utility/Cache.swift
+++ Bushel/Utility/Cache.swift
@@ -4,42 +4,42 @@ class Cache<Key, Value> where Key: Hashable {
     private let accessQueue = DispatchQueue(label: "Cache access")
     private var cache: [Key : Value] = [:]
     
-    func cached(for key: Key, orElse action: () throws -> Value) rethrows -> Value {
-        try accessQueue.sync {
-            cache[key]
-        } ?? {
-            let value = try action()
-            accessQueue.sync {
-                cache[key] = value
-            }
-            return value
-        }()
-    }
-    func cached(for key: Key, orElse action: () throws -> Value?) rethrows -> Value? {
-        try accessQueue.sync {
-            cache[key]
-        } ?? {
-            let value = try action()
-            if let value = value {
-                accessQueue.sync {
-                    cache[key] = value
-                }
-            }
-            return value
-        }()
-    }
-    
     subscript(_ key: Key) -> Value? {
         get {
-            cache[key]
+            accessQueue.sync {
+                cache[key]
+            }
         }
         set {
-            cache[key] = newValue
+            accessQueue.sync {
+                cache[key] = newValue
+            }
         }
     }
     
     func clear() {
         cache.removeAll()
+    }
+    
+}
+
+extension Cache {
+    
+    func `for`(_ key: Key, default action: @autoclosure () throws -> Value) rethrows -> Value {
+        try self[key] ?? {
+            let value = try action()
+            self[key] = value
+            return value
+        }()
+    }
+    func `for`(_ key: Key, default action: @autoclosure () throws -> Value?) rethrows -> Value? {
+        try self[key] ?? {
+            let value = try action()
+            if let value = value {
+                self[key] = value
+            }
+            return value
+        }()
     }
     
 }
